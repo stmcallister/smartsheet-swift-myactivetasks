d81b1a87bd70155de5e6b1b544059380d2a2ea04
Scott McAllister <stmcallister@gmail.com>
2017-06-30 10:48:00 -0700

    Initial Commit

---
+++ MyActiveTasks/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyActiveTasks
+//
+//  Created by Scott McAllister on 6/30/17.
+//  Copyright © 2017 ScottMcAllister. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyActiveTasks/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyActiveTasks
+//
+//  Created by Scott McAllister on 6/30/17.
+//  Copyright © 2017 ScottMcAllister. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
